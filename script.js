1f561e6f86031345fbd41da526ff82138500532c
fell-z <matheusfelipeagapito@hotmail.com>
2022-03-12 19:52:44 -0300

    Make equal button work

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*
+TODO:
+if a operator button is pressed after 1st entry, 2st entry and a operator has already been entered,
+evaluate the expression and insert the new operator after result and so on.
+*/
+
 // REFERENCE ELEMENTS
 const numButtons = document.querySelectorAll(".num-button");
 const operatorButtons = document.querySelectorAll(".operator-button");
@@ -5,39 +11,21 @@ const allClearButton = document.querySelector(".all-clear-button");
 const clearEntryButton = document.querySelector(".clear-entry-button");
 const decimalButton = document.querySelector(".decimal-button");
 const minusPlusButton = document.querySelector(".minus-plus-button");
+const equalButton = document.querySelector(".equal-button");
 
 const numbersDisplay = document.querySelector(".display .numbers");
 const previousEntryDisplay = document.querySelector(".display .previous-entry");
 
 // EVENT LISTENERS
-allClearButton.addEventListener("click", () => {
-  entrys.first = "";
-  entrys.second = "";
-  entrys.operator = "";
-  numbersDisplay.textContent = "";
-  previousEntryDisplay.textContent = "";
-});
+allClearButton.addEventListener("click", clearAllEntry);
 
-clearEntryButton.addEventListener("click", () => {
-  if (!entrys.operator) {
-    entrys.first = "";
-  } else {
-    entrys.second = "";
-  }
-  numbersDisplay.textContent = "";
-});
+clearEntryButton.addEventListener("click", clearEntry);
 
-decimalButton.addEventListener("click", () => {
-  if (!entrys.operator && !entrys.first.includes(".")) {
-    entrys.first += ".";
-    numbersDisplay.textContent += ".";
-  } else if (entrys.operator && !entrys.second.includes(".")) {
-    entrys.second += ".";
-    numbersDisplay.textContent += ".";
-  }
-});
+decimalButton.addEventListener("click", insertDecimal);
 
 minusPlusButton.addEventListener("click", switchSign);
+
+equalButton.addEventListener("click", evaluateEquation);
 
 operatorButtons.forEach((element) => {
   element.addEventListener("click", insertOperator);
@@ -48,6 +36,23 @@ numButtons.forEach((element) => {
 });
 
 // FUNCTIONS AND VARIABLES
+function clearAllEntry() {
+  entrys.first = "";
+  entrys.second = "";
+  entrys.operator = "";
+  numbersDisplay.textContent = "";
+  previousEntryDisplay.textContent = "";
+}
+
+function clearEntry() {
+  if (!entrys.operator) {
+    entrys.first = "";
+  } else {
+    entrys.second = "";
+  }
+  numbersDisplay.textContent = "";
+}
+
 function switchSign() {
   if (!entrys.operator) {
     if (entrys.first.includes("-")) {
@@ -73,6 +78,17 @@ const entrys = {
   operator: "",
   second: "",
 };
+
+function insertDecimal() {
+  if (!entrys.operator && !entrys.first.includes(".")) {
+    entrys.first += ".";
+    numbersDisplay.textContent += ".";
+  } else if (entrys.operator && !entrys.second.includes(".")) {
+    entrys.second += ".";
+    numbersDisplay.textContent += ".";
+  }
+}
+
 function insertOperator(event) {
   if (!entrys.operator) {
     entrys.operator = event.target.dataset.operator;
@@ -100,7 +116,16 @@ function insertNum(event) {
   }
 }
 
+function evaluateEquation() {
+  if (!entrys.operator) return;
+  const result = operate(entrys.first, entrys.second, entrys.operator);
+  clearAllEntry();
+  numbersDisplay.textContent = `${result}`;
+}
+
 function operate(x, y, operator) {
+  x = +x;
+  y = +y;
   switch (operator) {
     case "+":
       return x + y;
@@ -109,6 +134,7 @@ function operate(x, y, operator) {
     case "*":
       return x * y;
     case "/":
+      if (y === 0) return "Error";
       return x / y;
   }
 }
